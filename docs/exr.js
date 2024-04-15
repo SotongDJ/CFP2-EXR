faf2111da494402662d85e6d225cf6bd064c31de
David Soh <ln@trth.nl>
2024-04-15 16:52:16 +0000

    refactor: more modularize and change button list

--- docs/exr.js
+++ docs/exr.js
@@ -1,6 +1,20 @@
 final_title_str="CFP2-EXR 眼球地下電台播放室";
 final_root_path="https://xn--xp8h.xn--2os22eixx6na.xn--kpry57d/";
 final_artist_str="Eye X Radio 眼球地下電台";
+
+document.getElementById("aboutA").href="https://github.com/SotongDJ/CFP2-EXR";
+document.getElementById("emailA").href="mailto:github@stdj.me?subject=EXR 回饋信件&body=內容類型：（問題？建議？）%0A細節：（越詳細越好）";
+document.getElementById("infoA").href="https://github.com/SotongDJ/CFP2-EXR/wiki";
+document.getElementById("titleCSpan").innerText="眼球地下電台";
+document.getElementById("titleEmoji").innerText="👁";
+document.getElementById("titleESpan").innerText="EXR";
+document.getElementById("titleSec").href="https://www.instagram.com/eyexradio/";
+document.getElementById("titleSec").title="眼球地下電台";
+document.getElementById("sitleEmoji").innerText="👁";
+document.getElementById("sitleESpan").innerText="EXR";
+document.getElementById("sitleSec").href="https://www.instagram.com/eyexradio/";
+document.getElementById("sitleSec").title="眼球地下電台";
+
 show_apple=true;
 show_google=true;
 show_spotify=true;
